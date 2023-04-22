144c196faa14201a03247c355ba742669ce8fd8f
LuViKu <lukas.kuchernig@gmx.at>
2023-04-22 16:21:59 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestApplication.Models;
+
+namespace TestApplication.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("error")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/SearchController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplication.Controllers
+{
+    [Route("/search")]
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        public IActionResult PrintSearchParameters(string prefix, string location)
+        {
+            Console.WriteLine($"Successfully sent data to the backend. Prefix: {prefix} Location: {location}");
+            return Ok();
+        }
+    }
+}
